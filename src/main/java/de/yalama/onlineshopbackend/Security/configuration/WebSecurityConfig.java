1274e1f810b9400d2c96032d1069ac1ebc6e6919
aliyalama@googlemail.com <Gundam1979!>
2021-02-15 16:09:39 +0100

    security filter complete

--- src/main/java/de/yalama/onlineshopbackend/Security/configuration/WebSecurityConfig.java
+++ src/main/java/de/yalama/onlineshopbackend/Security/configuration/WebSecurityConfig.java
@@ -1,5 +1,16 @@
 package de.yalama.onlineshopbackend.Security.configuration;
 
-public class WebSecurityConfig {
-    //TODO
+import de.yalama.onlineshopbackend.Security.service.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
 }
