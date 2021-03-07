b0d18d72a822124e5dadb118bc088ae8fc54d0f1
aliyalama@googlemail.com <Gundam1979!>
2021-03-07 14:44:32 +0100

    filter methods

--- src/main/java/de/yalama/onlineshopbackend/shared/models/SearchQuery.java
+++ src/main/java/de/yalama/onlineshopbackend/shared/models/SearchQuery.java
@@ -1,11 +1,13 @@
 package de.yalama.onlineshopbackend.shared.models;
 
+import de.yalama.onlineshopbackend.Advertisement.model.Advertisement;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * This class holds all data relevant in searches for advertisements
@@ -14,12 +16,12 @@ import java.util.Set;
 @Setter
 @RequiredArgsConstructor
 public class SearchQuery {
-    private int minPrice;
-    private int maxPrice;
+    private Integer minPrice;
+    private Integer maxPrice;
     private String terms;
     private Set<String> searchTermsSet;
-    private int categoryId;
-    private int markeId;
+    private Long categoryId;
+    private Long markeId;
 
     public void feedSearchTermsToSet() {
         if(this.terms != null) {
@@ -29,5 +31,42 @@ public class SearchQuery {
                 this.searchTermsSet.add(term);
             }
         }
+    }
+
+    /**
+     * Sets advertisements null that do no match the query
+     * AtomicInteger shared by all filter methods to count the number of deleted ads for easier assembly later
+     */
+    private void filter(Advertisement[] ads, AtomicInteger deleted) {
+        for(int i = 0; i < ads.length; i++) {
+            if(ads[i] == null) {
+                continue;
+            }
+            Advertisement tempAd = ads[i];
+            boolean matchesPrice = this.matchesPrice(tempAd);
+            boolean matchesCategory
+        }
+    }
+
+    private boolean matchesCategory(Advertisement ad) {
+        return (this.categoryId == null) ? true : ad.getMarke().getCategory().getId() == this.getCategoryId();
+    }
+
+    private boolean matchesMarke(Advertisement ad) {
+        return (this.markeId == null) ? true : ad.getMarke().getId() == this.getMarkeId();
+    }
+
+    private boolean matchesPrice(Advertisement ad) {
+        return ad.getPrice() <= this.getMaxPrice() && ad.getPrice() >= this.getMinPrice();
+    }
+
+    private boolean adContainsSearchTerm(Advertisement ad) {
+        String[] adTerms = String.format("%s %s", ad.getTitle(), ad.getDescription()).toLowerCase().split(" ");
+        for(String term: adTerms) {
+            if(this.searchTermsSet.contains(term)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
